fdaa04f2e0683910c0df73c70107a3f140288f35
Yanick Champoux <yanick@babyl.ca>
2021-11-07 17:55:32 -0500

    storybook

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
-module.exports = {
-	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		adapter: '@sveltejs/adapter-static',
+import preprocess from 'svelte-preprocess';
 
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
 };
+
+export default config;
